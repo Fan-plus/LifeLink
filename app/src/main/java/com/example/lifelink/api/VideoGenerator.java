e21e4329bc92f76c4dae9dbdcf34b4af88853cb6
sleep-sleep-sheep <cld20051010@qq.com>
2026-06-28 23:26:59 +0800

    Use video service port 8081

--- app/src/main/java/com/example/lifelink/api/VideoGenerator.java
+++ app/src/main/java/com/example/lifelink/api/VideoGenerator.java
@@ -17,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class VideoGenerator {
     private static final String TAG = "VideoGenerator";
     private static final String SERVER_IP = "146.56.204.136"; // 您的服务器 IP
-    private static final String BASE_URL = "http://" + SERVER_IP + ":8080/";
+    private static final String BASE_URL = "http://" + SERVER_IP + ":8081/";
     private final MoneyPrinterApi api;
     private final Handler handler = new Handler(Looper.getMainLooper());
 
